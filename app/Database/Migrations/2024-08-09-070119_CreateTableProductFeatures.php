7246af8562c988468bdc4e517c70b4464c7e586f
Deshan Dissanayake <113162275+deshanbdissanayake@users.noreply.github.com>
2024-08-12 12:06:46 +0530

    20240812-1206

--- app/Database/Migrations/2024-08-09-070119_CreateTableProductFeatures.php
+++ app/Database/Migrations/2024-08-09-070119_CreateTableProductFeatures.php
@@ -8,7 +8,44 @@ class CreateTableProductFeatures extends Migration
 {
     public function up()
     {
-        $this->forge->
+        $this->forge->addField([
+            'prof_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'pro_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'null' => false,
+            ],
+            'icon' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            't_title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 150,
+                'null' => false,
+            ],
+            't_desc' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'status' => [
+                'type' => 'VARCHAR',
+                'constraint' => 11,
+                'default' => 'active',
+                'comment' => 'active/delete',
+            ],
+            'c_date' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+        ]);
+        $this->forge->addKey('prof_id', true);
+        $this->forge->createTable('product_features');
     }
 
     public function down()
--- app/Database/Migrations/2024-08-09-082910_CreateTableProductScreenShots.php
+++ app/Database/Migrations/2024-08-09-082910_CreateTableProductScreenShots.php
@@ -8,11 +8,39 @@ class CreateTableProductScreenShots extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'pss_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'pro_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'null' => false,
+            ],
+            'image' => [
+                'type' => 'TEXT',
+                'null' => false,
+            ],
+            'status' => [
+                'type' => 'VARCHAR',
+                'constraint' => 11,
+                'default' => 'active',
+                'comment' => 'active/delete',
+            ],
+            'c_date' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+        ]);
+        $this->forge->addKey('pss_id', true);
+        $this->forge->createTable('product_screenshots');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('product_screenshots');
     }
 }
